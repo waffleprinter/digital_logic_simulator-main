e9cb95f5dc2bb5a0e31960eaaa3c81d55e9e2601
waffleprinter <calaguipainchaudj@educ.college-montreal.qc.ca>
2024-04-17 14:56:18 -0400

    recognizes when you add switches and leds

--- Assets/Scripts/InstantiateGateScript.cs
+++ Assets/Scripts/InstantiateGateScript.cs
@@ -5,8 +5,13 @@ using UnityEngine;
 public class InstantiateGateScript : MonoBehaviour
 {
     [SerializeField] private GameObject gate;
+    private GameObject truthTableManager;
     private GameObject instantiatedGate;
     private bool instantiationStarted;
+
+    private void Start() {
+        truthTableManager = GameObject.Find("Truth Table Manager");
+    }
 
     private void Update() {
         if (!instantiationStarted) return;
@@ -15,6 +20,9 @@ public class InstantiateGateScript : MonoBehaviour
 
         if (instantiatedGate == null) {
             instantiatedGate = Instantiate(gate, mousePosition, Quaternion.identity);
+
+            if (gate.name == "SWITCH") truthTableManager.GetComponent<TruthTableManager>().switches.Add(instantiatedGate);
+            else if (gate.name == "LED") truthTableManager.GetComponent<TruthTableManager>().leds.Add(instantiatedGate);
         }
 
         if (Input.GetMouseButtonDown(0)) {
---
+++ Assets/Scripts/TruthTableManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TruthTableManager : MonoBehaviour
+{
+    public List<GameObject> switches = new List<GameObject>();
+    public List<GameObject> leds = new List<GameObject>();
+
+    void Start() {
+        
+    }
+
+    void Update() {
+        
+    }
+}
